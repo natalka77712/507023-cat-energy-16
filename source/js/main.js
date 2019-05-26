7a5d694776a7e46f7c9fd40d6648acb4da37d772
Евраз <kostousik@mail.ru>
2019-05-26 16:27:50 +0300

    Правки в соответствии с замечаниями

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,7 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 
-navMain.classList.remove('main-nav--nojs');
+navMain.classList.remove('main-nav--no-js');
 
 navToggle.addEventListener('click', function() {
   if (navMain.classList.contains('main-nav--closed')) {
